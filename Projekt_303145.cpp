944a0b983837de2a0f25aaedbeaa5f8aacb663db
sdabrowski03 <61691249+sdabrowski03@users.noreply.github.com>
2020-06-19 16:27:35 +0200

    Add files via upload

--- Projekt_303145.cpp
+++ Projekt_303145.cpp
@@ -1,18 +1,28 @@
 ﻿#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-struct wezel{
+#include <math.h>
+#include <ctype.h>
+
+#define CHUNK 1
+#define znakiAlFAB 26
+#define MAX 26
+#define iloscLITER 9 
+
+struct wezel {
     char znak = NULL;
-    float prawodopod = NULL;
+    double prawodopod = NULL;
     struct wezel* nastepny = NULL;
     struct wezel* lewy = NULL;
     struct wezel* prawy = NULL;
 };
-struct kod {
+
+struct kod_bitowo {
     char znak = NULL;
-    char kod_dla_znaku[100] = "";
+    char kod_dla_znaku[26] = "";
 };
-wezel stworzLisc(float war, int znak){
+
+wezel stworzLisc(double war, int znak) {
     struct wezel tmp;
     if (war > 0)
         tmp.prawodopod = war;
@@ -26,34 +36,60 @@ wezel stworzLisc(float war, int znak){
 
     return tmp;
 }
+void licz_znaki(const char* ciag, int* liczba_znakow, int rozmiar) {
+    char znak = NULL;
+    for (int k = 0; k < rozmiar - 1; k++) {
+        znak = *(ciag + k);
+        *(liczba_znakow + (znak - 97)) = *(liczba_znakow + (znak - 97)) + 1;
+    }
+}
+void licz_prawdopodobienstwo(int* liczba_znakow, double* prawodopod, int rozmiar, int* ile_liter) {
+    for (int k = 0; k < 26; k++) {
+        if (liczba_znakow[k] > 0) {
+            prawodopod[k] = (double(liczba_znakow[k]) / double(rozmiar - 1));
+        }
+        else
+            prawodopod[k] = 0.00;
+        if (prawodopod[k] > 0) *(ile_liter) = *(ile_liter)+1;
+    }
+}
+void stworz_tabele_wezlow(wezel* tabela_wezlow, double* prawodopod) {
+    for (int k = 0; k < 26; k++) {
+        tabela_wezlow[k] = stworzLisc(prawodopod[k], k);
+    }
+}
+
+
 void sorotwanieStructury(wezel* tabela, int n) {
     int i = 0, j = 0;
-    float tmp = 0;
+    double tmp = 0;
     char znk = 0;
     for (i = 0; i < n; i++) {
         for (j = i + 1; j < n; j++) {
-            if ((tabela[j].prawodopod) < (tabela[i].prawodopod) && tabela[i].prawodopod >= 0){
-                 tmp = (tabela[i].prawodopod);
-                 znk = (tabela[i].znak);
-                 (tabela[i].prawodopod) = (tabela[j].prawodopod);
-                 (tabela[i].znak) = (tabela[j].znak);
-                 (tabela[j].prawodopod) = tmp;
-                 (tabela[j].znak) = znk;
+            if ((tabela[j].prawodopod) < (tabela[i].prawodopod) && tabela[i].prawodopod >= 0) {
+                tmp = (tabela[i].prawodopod);
+                znk = (tabela[i].znak);
+                (tabela[i].prawodopod) = (tabela[j].prawodopod);
+                (tabela[i].znak) = (tabela[j].znak);
+                (tabela[j].prawodopod) = tmp;
+                (tabela[j].znak) = znk;
             }
             else;
         }
     }
 }
-void polacz_wezly_w_liste( struct wezel* element_1, struct wezel* element_2){
-    if(element_2->prawodopod != 0)
+
+void polacz_wezly_w_liste(struct wezel* element_1, struct wezel* element_2) {
+    if (element_2->prawodopod != 0)
         element_1->nastepny = *&(element_2);
     else
     {
         element_1->nastepny = NULL;
     }
 }
-void zbudujStruktureDrzewsiasta(struct wezel *&root){
-    struct wezel* wezel_tmp, * r, * v1, *v2;
+
+void zbudujStruktureDrzewsiasta(struct wezel*& root) {
+    struct wezel* wezel_tmp, * r, * v1, * v2;
     while (true)
     {
         v1 = root;
@@ -69,9 +105,9 @@ void zbudujStruktureDrzewsiasta(struct wezel *&root){
         wezel_tmp->prawy = v2;
         wezel_tmp->prawodopod = v1->prawodopod + v2->prawodopod;
 
-        if (root == 0 || (wezel_tmp->prawodopod <= root->prawodopod)){
+        if (root == 0 || (wezel_tmp->prawodopod <= root->prawodopod)) {
             wezel_tmp->nastepny = root;
-            root = wezel_tmp;   
+            root = wezel_tmp;
             continue;
         }
         r = root;
@@ -80,12 +116,13 @@ void zbudujStruktureDrzewsiasta(struct wezel *&root){
             r = r->nastepny;
         }
 
-            wezel_tmp->nastepny = r->nastepny;
-            r->nastepny = wezel_tmp;
+        wezel_tmp->nastepny = r->nastepny;
+        r->nastepny = wezel_tmp;
     }
 }
-void wystwietlZnak(struct wezel* root, char znak, char *kod, struct kod* kopia_kodu) {
-    char kopia[100] = "";
+
+void wystwietlZnak(struct wezel* root, char znak, char* kod, struct kod_bitowo* kopia_kodu) {
+    char kopia[MAX] = "";
     char lz[2] = "0";
     char pz[2] = "1";
     if (root != NULL && znak == root->znak) {
@@ -101,106 +138,158 @@ void wystwietlZnak(struct wezel* root, char znak, char *kod, struct kod* kopia_k
             wystwietlZnak(root->lewy, znak, kod, kopia_kodu);
         }
         strcpy(kod, kopia);
-        if (root != NULL){
+        if (root != NULL) {
             strncat(kod, pz, 1);
             wystwietlZnak(root->prawy, znak, kod, kopia_kodu);
         }
     }
 }
-int main()
-{
-    char znak;
-    int liczba_znakow[26] = { 0 };
-    float prawodopod[26] = { 0 };
-    float dlugosc = 0;
-    int ile_liter = 0;
-    struct wezel tabelaLISCI[26];
-    struct kod tabelaKODOW[26];
-
-    char ciag1[] = "aaabbbcccddd";
-
-    printf("Podany zostal ciag:  ");
-    printf(ciag1); printf("\n");
-
-
-
-            dlugosc = (sizeof(ciag1)) - 1;
-
-            for (int k = 0; k < dlugosc; k++) {
-                znak = ciag1[k];
-                liczba_znakow[znak - 97]++;
-            }
-            for (int k = 0; k < 26; k++) {
-                if (liczba_znakow[k] > 0) {
-                    prawodopod[k] = (liczba_znakow[k] / dlugosc);
-                    ile_liter++;
-                }
-                else
-                    prawodopod[k] = 0.00;
-            }
-    /* for (int k = 0; k < 26; k++) {
-         czyJeden += prawodopod[k];
-     }
-
-    for (int m = 0; m < 26; m++) {
-        printf("Znak %c wystepuje: %d \n", (m + 97), liczba_znakow[m]);
+int convert(int n) {
+    int dec = 0, i = 0, rem;
+    while (n != 0) {
+        rem = n % 10;
+        n /= 10;
+        dec += rem * pow(2, i);
+        ++i;
     }
-    printf("\n");
-    
-
-            
-
-            for (int k = 0; k < 26; k++) {
-               
-                printf("Prawdopodobienstwo dla %c wynosi %f \n", tabelaLISCI[k].znak, tabelaLISCI[k].prawodopod);
-            }
-            printf("\n");
-
-       
-
-            printf("PO SORTOWANIU \n");
-            printf("\n");
-
-           for (int k = 0; k < 26; k++) {
-
-               printf("Prawdopodobienstwo dla %c wynosi %f \n", tabelaLISCI[k].znak, tabelaLISCI[k].prawodopod);
-           }
-*/
-            for (int k = 0; k < 26; k++) {
-
-                tabelaLISCI[k] = stworzLisc(prawodopod[k], k);
-
-            }
-
-            sorotwanieStructury(tabelaLISCI, 26);
-
-           for (int i = 0; i < 25; i++) {
-               if (tabelaLISCI[i].prawodopod > 0)
-                   polacz_wezly_w_liste(&tabelaLISCI[i], &tabelaLISCI[i + 1]);
-           }
-
-           struct wezel* root = (struct wezel*)malloc(sizeof(*root));
-           root = &tabelaLISCI[26-ile_liter];
-         
-           zbudujStruktureDrzewsiasta(root);
-
-           for (int z = 0; z <= 26; z++) {
-               char kod[100] = "";
-               char const zerujKod[100] = "";
-               strcpy(kod, zerujKod);
-               if (liczba_znakow[z] > 0) {
-                   char zn = z + 97;
-                    wystwietlZnak(root, zn, kod,&tabelaKODOW[z]);
-               }
-           }
-
-           printf("Podany ciag w postaci skompresowanej: \n");
-           for (int i = 0; i <= dlugosc; i++) {
-               int war = ciag1[i] - 97;
-              printf("%s", tabelaKODOW[war].kod_dla_znaku);
-           }
-
-    return 0;
-
+    return dec;
 }
 
+int main()
+{
+    struct wezel tabela_wezlow[26];
+    struct kod_bitowo tabela_kodow[znakiAlFAB];
+    FILE* ciag_skompresowany = fopen("ciag_skompresowany.txt", "w");
+    FILE* ciag_skompresowany_zakodowany = fopen("ciag_skompresowany_zakodowany.txt", "w");
+    FILE* tekst = fopen("tekst.txt", "r");
+    char znak;
+    int  pozycja = 0;
+    int  rozmiar = 0;
+    int ile_liter = 0;
+    int liczba_bitow = 0;
+    char* ciag = NULL;
+    char* ciag_tmp = NULL;
+    ciag = (char*)malloc(1 * sizeof(*ciag));
+
+    while (1) {
+
+        znak = fgetc(tekst);
+        znak = tolower(znak);
+        if ((znak >= 97 && znak <= 122) || znak == EOF) {
+            if (znak == EOF) {
+                rozmiar += 1;
+                ciag_tmp = (char*)realloc(ciag, rozmiar);
+                ciag = ciag_tmp;
+                ciag[pozycja++] = '\0';
+                break;
+            }
+            if (rozmiar <= pozycja) {
+                rozmiar += 1;
+                ciag_tmp = (char*)realloc(ciag, rozmiar);
+                if (!ciag_tmp) {
+                    free(ciag);
+                    ciag_tmp = NULL;
+                    break;
+                }
+                ciag = ciag_tmp;
+            }
+            ciag[pozycja++] = znak;
+        }
+        else continue;
+    }
+    printf("Ciag: %s\n", ciag);
+    printf("Liczba obliczonych znakow to: %d\n", rozmiar - 1);
+
+    int  liczba_znakow[26] = { 0 };
+    double prawodopod[26] = { 0 };
+
+    licz_znaki(ciag, liczba_znakow, rozmiar);
+    licz_prawdopodobienstwo(liczba_znakow, prawodopod, rozmiar, &ile_liter);
+    stworz_tabele_wezlow(tabela_wezlow, prawodopod);
+    sorotwanieStructury(tabela_wezlow, znakiAlFAB);
+
+    for (int i = 0; i < znakiAlFAB - 1; i++) {
+        if (tabela_wezlow[i].prawodopod > 0)
+            polacz_wezly_w_liste(&tabela_wezlow[i], &tabela_wezlow[i + 1]);
+    }
+
+    struct wezel* root = (struct wezel*)malloc(sizeof(*root));
+    root = &tabela_wezlow[znakiAlFAB - ile_liter];
+    zbudujStruktureDrzewsiasta(root);
+
+    for (int z = 0; z < znakiAlFAB; z++) {
+        char kod[MAX] = "";
+        char const zerujKod[MAX] = "";
+        strcpy(kod, zerujKod);
+        if (liczba_znakow[z] > 0) {
+            char zn = z + 97;
+            wystwietlZnak(root, zn, kod, &tabela_kodow[z]);
+        }
+    }
+    for (int i = 0; i < rozmiar - 1; i++) {
+        int war = ciag[i] - 97;
+        fprintf(ciag_skompresowany, "%s", tabela_kodow[war].kod_dla_znaku);
+    }
+
+    fclose(ciag_skompresowany);
+    fclose(tekst);
+    ciag_skompresowany = fopen("ciag_skompresowany.txt", "r");
+
+    rozmiar = 0;
+    pozycja = 0;
+
+    char kompresja[9] = "";
+    char kompresja_zerowanie[9] = "";
+
+    int osiem = 1;
+    znak = NULL;
+    while (1) {
+
+        znak = fgetc(ciag_skompresowany);
+        if (znak == EOF) {
+            if (osiem > 1) {
+                while (osiem <= 8 && osiem > 1) {
+                    char uzupelnienie = '1';
+                    strncat(kompresja, &uzupelnienie, 1);
+                    osiem++;
+                }
+                printf("8_bit:%s     ", kompresja);
+                int x = atoi(kompresja);
+                printf("Wartosc: %d      ", convert(x));
+                printf("Znak dla wartosci: %c \n", convert(x));
+                fprintf(ciag_skompresowany_zakodowany, "%c", convert(x));
+            }
+            break;
+        }
+        liczba_bitow++;
+        strncat(kompresja, &znak, 1);
+        if (osiem == 8) {
+            printf("8_bit:%s     ", kompresja);
+            int x = atoi(kompresja);
+            printf("Wartosc: %d      ", convert(x));
+            printf("Znak dla wartosci: %c \n", convert(x));
+            fprintf(ciag_skompresowany_zakodowany, "%c", convert(x));
+            strcpy(kompresja, kompresja_zerowanie);
+            osiem = NULL;
+
+        }
+        osiem++;
+    }
+
+    int bity = 0;
+    znak = NULL;
+
+
+
+    fclose(ciag_skompresowany);
+    remove("ciag_skompresowany.txt");
+    fclose(ciag_skompresowany_zakodowany);
+    ciag_skompresowany_zakodowany = fopen("ciag_skompresowany_zakodowany.txt", "a");
+    fprintf(ciag_skompresowany_zakodowany, "#CS%d", liczba_bitow);
+    fclose(ciag_skompresowany_zakodowany);
+
+
+
+    free(ciag);
+    return 0;
+}
